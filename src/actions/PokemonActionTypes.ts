dea10ea717dfc922860b8970632aff6abc6d568b
Ashley Shen <ashleyshen101@gmail.com>
2022-07-06 17:04:56 -0700

    Update the types that needed in this project

--- src/actions/PokemonActionTypes.ts
+++ src/actions/PokemonActionTypes.ts
@@ -2,8 +2,26 @@ export const POKEMON_LOADING = "POKEMON_LOADING";
 export const POKEMON_FAIL = "POKEMON_FAIL";
 export const POKEMON_SUCCESS = "POKEMON_SUCCESS";
 
-interface PokemonLoading {
+type PokemonAbility = {
+  ability: {
+    name: string
+    url: string
+  }
+}
+
+type PokemonSprites = {
+  front_default: string
+}
+
+type PokemonLoading {
   type: typeof POKEMON_LOADING;
+}
+
+type PokemonStat = {
+  base_stat: number
+  stat: {
+    name: string
+  }
 }
 
 interface PokemonFail {
@@ -11,5 +29,12 @@ interface PokemonFail {
 }
 
 interface PokemonSuccess {
-  type: typeof POKEMON_SUCCESS;
+  type: typeof POKEMON_SUCCESS, 
+  payload: {
+    abilities: PokemonAbility[],
+    sprites: PokemonSprites,
+    stats: PokemonStat[] 
+  }
 }
+
+type PokemonDispatchTypes = PokemonLoading | PokemonFail | PokemonSuccess;
